faf0027d163c96929f19975b3a1c984e5f3a20b1
四季天书SkiTiSu <sjtsjjy@gmail.com>
2017-11-04 00:19:51 +0800

    First commit

---
+++ StylusPressureLevelsTest/MainWindow.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace StylusPressureLevelsTest
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        float lastPreesure = 0;
+        float minDifference = 1;
+        private void Grid_StylusMove(object sender, StylusEventArgs e)
+        {
+            var pts = e.GetStylusPoints(null);
+            foreach (var p in pts)
+            {
+                rect.Opacity = p.PressureFactor;
+                textBlockCurrentPressure.Text = (p.PressureFactor * 100).ToString("#.####") + "%";
+                if (Math.Abs(p.PressureFactor - lastPreesure) != 0 && Math.Abs(p.PressureFactor - lastPreesure) < minDifference)
+                {
+                    minDifference = Math.Abs(p.PressureFactor - lastPreesure);
+                    textBlockPossiblePressure.Text = (1 / minDifference).ToString("#.#");
+                }
+                lastPreesure = p.PressureFactor;
+            }
+        }
+    }
+}
